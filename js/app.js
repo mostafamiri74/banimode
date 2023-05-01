a05736260bdeb80e86b4f605b5d706a91b2d7b49
mostafa miri <mostafamiri12345@gmail.com>
2023-05-02 01:19:18 +0330

    Add baners and sliders whitout pagination

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper-left-top", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".swiper-right-down", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
