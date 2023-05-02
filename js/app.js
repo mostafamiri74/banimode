21ec55dd6b30de6f2c720dfb445e0c1888ba2321
hesam-valizadeh <hesam.valizadeh@gmail.com>
2023-05-02 12:10:09 +0330

    Add feature loop slider Add feature autoplay slider

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 var swiper = new Swiper(".swiper-left-top", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
+    clickable: true 
   },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -9,10 +15,17 @@ var swiper = new Swiper(".swiper-left-top", {
   },
 });
 
+
 var swiper = new Swiper(".swiper-right-down", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
+    clickable: true 
   },
   navigation: {
     nextEl: ".swiper-button-next",
